ffa45c2b915e4be21e84c394f994038720139eeb
jimboweb <jimboweb@gmail.com>
2019-05-25 23:07:37 -0400

    added project

---
+++ springgradleproject/src/test/java/nyc/jimnstewart/springgradleproject/SpringgradleprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package nyc.jimnstewart.springgradleproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringgradleprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
